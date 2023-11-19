683cf6dd42d2249cfb6a1676e8c82fc455ac6b8c
shobhit7618 <shobhityadav9919@gmail.com>
2023-11-19 11:45:06 +0530

    .

--- frontend/src/components/main/Signup.jsx
+++ frontend/src/components/main/Signup.jsx
@@ -2,49 +2,130 @@ import React from 'react'
 
 const Signup = () => {
     return (
-        <div>
-            <section class="bg-gray-50 dark:bg-gray-900">
-                <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-                    <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
-                        <img class="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo"/>
-                            Flowbite
-                    </a>
-                    <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-                        <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-                            <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                                Create Account
-                            </h1>
-                            <form class="space-y-4 md:space-y-6" action="#">
-                                <div>
-                                    <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                                    <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required=""/>
-                                </div>
-                                <div>
-                                    <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                                    <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""/>
-                                </div>
-                                <div>
-                                    <label for="confirm-password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm password</label>
-                                    <input type="confirm-password" name="confirm-password" id="confirm-password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""/>
-                                </div>
-                                <div class="flex items-start">
-                                    <div class="flex items-center h-5">
-                                        <input id="terms" aria-describedby="terms" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required="" />
-                                    </div>
-                                    <div class="ml-3 text-sm">
-                                        <label for="terms" class="font-light text-gray-500 dark:text-gray-300">I accept the <a class="font-medium text-primary-600 hover:underline dark:text-primary-500" href="#">Terms and Conditions</a></label>
-                                    </div>
-                                </div>
-                                <button type="submit" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Create an account</button>
-                                <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-                                    Already have an account? <a href="#" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Login here</a>
-                                </p>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </section>
+        <div className="h-full bg-gray-400 dark:bg-gray-900">
+  <div className="mx-auto">
+    <div className="flex justify-center px-6 py-12">
+      <div className="w-full xl:w-3/4 lg:w-11/12 flex">
+        <div
+          className="w-full h-auto bg-gray-400 dark:bg-gray-800 hidden lg:block lg:w-5/12 bg-cover rounded-l-lg"
+          style={{
+            backgroundImage:
+              'url("https://cdn.pixabay.com/photo/2017/03/28/12/06/chairs-2181916_1280.jpg")'
+          }}
+        />
+        <div className="w-full lg:w-7/12 bg-white dark:bg-gray-700 p-5 rounded-lg lg:rounded-l-none">
+          <h3 className="py-4 text-2xl text-center text-gray-800 dark:text-white">
+            Create an Account!
+          </h3>
+          <form className="px-8 pt-6 pb-8 mb-4 bg-white dark:bg-gray-800 rounded">
+            <div className="mb-4 md:flex md:justify-between">
+              <div className="mb-4 md:mr-2 md:mb-0">
+                <label
+                  className="block mb-2 text-sm font-bold text-gray-700 dark:text-white"
+                  htmlFor="firstName"
+                >
+                  First Name
+                </label>
+                <input
+                  className="w-full px-3 py-2 text-sm leading-tight text-gray-700 dark:text-white border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                  id="firstName"
+                  type="text"
+                  placeholder="First Name"
+                />
+              </div>
+              <div className="md:ml-2">
+                <label
+                  className="block mb-2 text-sm font-bold text-gray-700 dark:text-white"
+                  htmlFor="lastName"
+                >
+                  Last Name
+                </label>
+                <input
+                  className="w-full px-3 py-2 text-sm leading-tight text-gray-700 dark:text-white border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                  id="lastName"
+                  type="text"
+                  placeholder="Last Name"
+                />
+              </div>
+            </div>
+            <div className="mb-4">
+              <label
+                className="block mb-2 text-sm font-bold text-gray-700 dark:text-white"
+                htmlFor="email"
+              >
+                Email
+              </label>
+              <input
+                className="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 dark:text-white border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                id="email"
+                type="email"
+                placeholder="Email"
+              />
+            </div>
+            <div className="mb-4 md:flex md:justify-between">
+              <div className="mb-4 md:mr-2 md:mb-0">
+                <label
+                  className="block mb-2 text-sm font-bold text-gray-700 dark:text-white"
+                  htmlFor="password"
+                >
+                  Password
+                </label>
+                <input
+                  className="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 dark:text-white border border-red-500 rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                  id="password"
+                  type="password"
+                  placeholder="******************"
+                />
+                <p className="text-xs italic text-red-500">
+                  Please choose a password.
+                </p>
+              </div>
+              <div className="md:ml-2">
+                <label
+                  className="block mb-2 text-sm font-bold text-gray-700 dark:text-white"
+                  htmlFor="c_password"
+                >
+                  Confirm Password
+                </label>
+                <input
+                  className="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 dark:text-white border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                  id="c_password"
+                  type="password"
+                  placeholder="******************"
+                />
+              </div>
+            </div>
+            <div className="mb-6 text-center">
+              <button
+                className="w-full px-4 py-2 font-bold text-white bg-blue-500 rounded-full hover:bg-blue-700 dark:bg-blue-700 dark:text-white dark:hover:bg-blue-900 focus:outline-none focus:shadow-outline"
+                type="button"
+              >
+                Register Account
+              </button>
+            </div>
+            <hr className="mb-6 border-t" />
+            <div className="text-center">
+              <a
+                className="inline-block text-sm text-blue-500 dark:text-blue-500 align-baseline hover:text-blue-800"
+                href="#"
+              >
+                Forgot Password?
+              </a>
+            </div>
+            <div className="text-center">
+              <a
+                className="inline-block text-sm text-blue-500 dark:text-blue-500 align-baseline hover:text-blue-800"
+                href="./index.html"
+              >
+                Already have an account? Login!
+              </a>
+            </div>
+          </form>
         </div>
+      </div>
+    </div>
+  </div>
+</div>
     )
 }
 
